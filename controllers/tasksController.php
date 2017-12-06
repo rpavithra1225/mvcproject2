8cb8a10b5444c8d7074374bf1ccd6dfe2024cda6
Pavithra Rajan <pr384@njit.edu>
2017-12-06 08:27:05 -0500

    update show_task with form to detail out id details

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -70,7 +70,7 @@ class tasksController extends http\controller
     {
         $record = todos::findOne($_REQUEST['id']);
         $record->delete();
-        print_r($_POST);
+        header("Location: index.php?page=tasks&action=all");
 
     }
 
--- pages/show_task.php
+++ pages/show_task.php
@@ -23,6 +23,16 @@
 //print_r($data);
 ?>
 
+<form action="index.php?page=tasks&action=save&id=<?php echo $data->id; ?>" method="post">
+
+    ID: <input type="text" name="id" value="<?php echo $data->id; ?>"><br>
+    Email: <input type="text" name="email" value="<?php echo $data->owneremail; ?>"><br>
+    Task Created Date: <input type="text" name="createddate" value="<?php echo $data->createddate; ?>"><br>
+    Task Due Date: <input type="text" name="duedate" value="<?php echo $data->duedate; ?>"><br>
+    Task Message: <input type="text" name="message" value="<?php echo $data->message; ?>"><br>
+    Whether task has been completed?: <input type="text" name="isdone" value="<?php echo $data->isdone; ?>"><br>
+    <input type="submit" value="Submit form">
+</form>
 
 <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
     <button type="submit" form="form1" value="delete">Delete</button>
