f40b613370c0c1c6455a14a92824e5cfdac61e18
Pavithra Rajan <pr384@njit.edu>
2017-12-16 14:51:12 -0500

    fixed bugs in formatting

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,8 +1,35 @@
-<?php include 'header.php';
+<?php
+include 'header.php';
+?>
+
+<div class="overlay"></div>
+
+<div class="masthead">
+    <div class="masthead-bg"></div>
+    <div class="container h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="masthead-content text-white py-5 py-md-0">
+                    <h1 class="mb-3">To=Doist</h1>
+                    <p class="mb-5">An easy way to organize your to-do lists</p>
+                    <div class="input-group input-group-newsletter">
+
+                  <button class="btn btn-primary" type="button" style="background: #06425C ">Get Started</button>
+                </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+/*<?php include 'header.php';
 
     session_start();
     if(isset($_SESSION['userID'])) {
-        echo $_SESSION['userID'];
+        //echo $_SESSION['userID'];
         include 'all_tasks.php';
     }
      else{
@@ -10,4 +37,4 @@
     }
     ?>
 
-<?php include 'footer.php'?>
+<?php include 'footer.php'?>*/
