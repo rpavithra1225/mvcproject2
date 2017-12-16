713adf0b1a290f84b11f323f94ab4fe9040c172e
Pavithra Rajan <pr384@njit.edu>
2017-12-16 15:11:27 -0500

    fixed homepage bugs

--- pages/homepage.php
+++ pages/homepage.php
@@ -2,39 +2,25 @@
 include 'header.php';
 ?>
 
-<div class="overlay"></div>
-
-<div class="masthead">
-    <div class="masthead-bg"></div>
-    <div class="container h-100">
-        <div class="row h-100">
-            <div class="col-12 my-auto">
-                <div class="masthead-content text-white py-5 py-md-0">
-                    <h1 class="mb-3">To=Doist</h1>
-                    <p class="mb-5">An easy way to organize your to-do lists</p>
-                    <div class="input-group input-group-newsletter">
-
-                  <button class="btn btn-primary" type="button" style="background: #06425C ">Get Started</button>
-                </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<body background="img/aqua.jpg" style="height: 100%; background-position: center; background-repeat: no-repeat; background-size: cover;">
 
 
+</body>
+</html>
 
-/*<?php include 'header.php';
 
-    session_start();
-    if(isset($_SESSION['userID'])) {
+<?php
+//include 'header.php';
+
+    //session_start();
+    //if(isset($_SESSION['userID'])) {
         //echo $_SESSION['userID'];
-        include 'all_tasks.php';
-    }
-     else{
-        include 'login.php';
-    }
-    ?>
+      //  include 'all_tasks.php';
+    //}
+     //else{
+       // include 'login.php';
+    //}
+    //
 
-<?php include 'footer.php'?>*/
+// include 'footer.php'
+    ?>
