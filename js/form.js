68d725b558cd0044ccf8cb58e255d1ac76af6cb4
Pavithra Rajan <pr384@njit.edu>
2017-12-17 02:10:12 -0500

    fixed bugs related to datepicker cleaned jquery lib links

---
+++ js/form.js
@@ -0,0 +1,14 @@
+
+$('#duedate').datepicker({
+    //uiLibrary:  'bootstrap4'
+    singleDatePicker: true,
+    //autoUpdateInput: true,
+    showDropdowns: true,
+    "locale": {
+        "format": "YYYY-MM-DD",
+        "separator": "-",
+    }
+});
+
+$('#duedate').val('')
+
--- pages/navbar.php
+++ pages/navbar.php
@@ -1,5 +1,17 @@
 <head>
+    <!--
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gijgo/1.7.0/combined/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/gijgo/1.7.0/combined/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script> -->
+
+
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/table.css">
     <link rel="stylesheet" href="css/form.css">
@@ -40,7 +52,6 @@
         </ul>
     </div>
 </nav>
-</body>
 
 <main role="main" class="container" >
     <div class="starter-template">
